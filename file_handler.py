562bee629afb05ae640145d5b2537f3d20be9ce3
Rick Dawson <rick.dawson@blackhorsesolutions.com>
2021-03-03 14:35:04 -0500

    Added file_handler for work

---
+++ file_handler.py
@@ -0,0 +1,12 @@
+# Gets addresses from a textfile
+
+# select a file
+
+# Open the file
+
+# Read the contents into a list
+list = file contents
+
+# close the file
+
+# print the contents of the file
